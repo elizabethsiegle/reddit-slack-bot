e491967f4af580d62356d6781c46144ca457f330
Elizabeth Siegle <lizzie.siegle@gmail.com>
2017-04-28 12:24:07 -0400

    FORMATSTRING FOR FORMATTED MESSAGE GAAH

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE PatternGuards #-}
@@ -8,6 +9,7 @@
 module Main where
 
 import System.IO
+import GHC.Types (IO (..))
 import Control.Monad
 import Data.Char(toUpper)
 import Data.Maybe
@@ -17,9 +19,11 @@ import Prelude
 import Network.HTTP.Client.TLS (getGlobalManager)
 import Network.HTTP.Client
 import Network.Linklater
+import Control.Monad.Trans.Maybe
 --import Network.HTTP.Client.TLS
 --import System.IO.Encoding
 import Data.Aeson
+import Servant.Common.Req
 
 import qualified Data.Text as T
 --import qualified Network.Images.Search as Search
@@ -75,51 +79,48 @@ parseText text = case T.strip text of
   "" -> Nothing
   x -> Just x
 
-liftMaybe :: Maybe a -> MaybeT IO a
+liftMaybe :: Maybe a -> IO a
 liftMaybe = maybe mzero return
 
---messageOfCommand :: Command -> MaybeT IO Message
+messageOfCommand :: Command -> IO Message--IO (Either Servant.Common.Req.ServantError YQLResponse)
+messageOfCommand (Command "stock" user channel (Just text)) = do
+  --gapi <- liftIO googleConfigIO
+  manager <- getGlobalManager
+  query <- liftMaybe (parseText text)
+  res <- runClientM (getQuotes (YQLQuery [StockSymbol query]) ) (ClientEnv manager yahooFinanceJsonBaseUrl)
+  --url <- liftMaybe (listToMaybe res)
+  --return res
+  return (messageOf [FormatAt user, FormatString res])
+  where
+    messageOf =
+      FormattedMessage (EmojiIcon "gift") "stockbot" channel
+messageOfCommand _ =
+  mzero
 
---messageOfCommand (Command "stock" user channel (Just text)) = do
---  --gapi <- liftIO googleConfigIO
---  manager <- getGlobalManager
---  query <- liftMaybe (parseText text)
---  --urls <- liftIO (Search.linksOfQuery gapi query)
---  res <- runClientM (getQuotes (YQLQuery [StockSymbol query]) ) (ClientEnv manager yahooFinanceJsonBaseUrl)
---  url <- liftMaybe (listToMaybe res)
---  return (messageOf [FormatAt user, FormatLink url url])
---  where
---    messageOf =
---      FormattedMessage (EmojiIcon "gift") "stockbot1" channel
---messageOfCommand _ =
---  mzero
+stockify :: Maybe Command -> IO Text
+stockify Nothing =
+  return "Unrecognized Slack request!"
 
---jpgto :: Maybe Command -> IO Text
---jpgto Nothing =
---  return "Unrecognized Slack request!"
-
---jpgto (Just command) = do
---  putStrLn ("+ Incoming command: " <> show command)
---  manager <- getGlobalManager
---  res <- runClientM (getQuotes (YQLQuery [StockSymbol res]) ) (ClientEnv manager yahooFinanceJsonBaseUrl)
---  config <- configIO
---  message <- (runMaybeT . messageOfCommand) res
---  putStrLn ("+ Outgoing message: " <> (encode <$> res)) --message, !res
---  case (debug, res) of
---    (False, Just m) -> do
---      _ <- say m config
---      return ""
---    (False, Nothing) ->
---      return "ERROR PROCESSING INPUT"
---    _ ->
---      return ""
---  where
---    debug = False
+stockify (Just command) = do
+  putStrLn ("+ Incoming command: " <> show command)
+  message <- (messageOfCommand) command
+  config <- configIO
+  putStrLn ("+ Outgoing message: " <> show (encode <$> message))
+  case (debug, message) of
+    (False, Just m) -> do
+      _ <- say m config
+      return ""
+    (False, Nothing) ->
+      return "error ugh poop"
+    _ ->
+      return ""
+  where
+    debug = False
 
 
 main :: IO ()
 main = do
   putStrLn ("+ Listening on port " <> show port)
-  -- run port (slashSimple jpgto)
+  run port (slashSimple stockify)
     where
-      port = 3333
+      port = 3000
