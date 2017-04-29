7a812dce7d8e8cab65569a985290656ae3ea0853
Elizabeth Siegle <lizzie.siegle@gmail.com>
2017-04-28 23:17:51 -0400

    BLESS UP IT SHOWS STOCK INFO

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE PatternGuards #-}
@@ -9,6 +10,8 @@
 module Main where
 
 import System.IO
+import Data.ByteString.Lazy.Char8
+import Data.ByteString.Lazy.Char8()
 import GHC.Types (IO (..))
 import Control.Monad
 import Data.Char(toUpper)
@@ -20,14 +23,10 @@ import Network.HTTP.Client.TLS (getGlobalManager)
 import Network.HTTP.Client
 import Network.Linklater
 import Control.Monad.Trans.Maybe
---import Network.HTTP.Client.TLS
---import System.IO.Encoding
 import Data.Aeson
 import Servant.Common.Req
 
 import qualified Data.Text as T
---import qualified Network.Images.Search as Search
---import qualified Data.ByteString as B
 import           Control.Monad.IO.Class (liftIO)
 import           Control.Monad.Trans.Maybe (MaybeT, runMaybeT)
 import           Data.Aeson (encode)
@@ -35,22 +34,7 @@ import           Data.Text (Text)
 import Network.HTTP.Types
 import Network.TLS
 import Network.Wai.Handler.Warp (run)
---import Network.Wai
---import Network.Wai.Handler.Warp
---import Network.Wai.Handler.WarpTLS
---import Network.Wai.Handler.Warp.Run 
---import Control.Lens
 import Network.Wreq
---import Data.Word8
---import Data.Attoparsec
---import HTTP.Conduit
----- Naked imports.
---import           BasePrelude hiding (words, intercalate)
-
-----import System.IO.UTF8
-----import Data.Char  
---import Data.Char(toUpper)
---import Control.Monad.Trans.Class
 
 ----import Web.HackerNews
 import Network.HTTP.Types.Status (statusCode)
@@ -62,17 +46,13 @@ import Web.Yahoo.Finance.YQL
 ----import Network.Yahoo.Finance
 
 
-cleverlyReadFile :: FilePath -> IO Text
-cleverlyReadFile filename =
-  T.filter (/= '\n') . T.pack <$> readFile filename
+readSlackFile :: FilePath -> IO Text
+readSlackFile filename =
+  T.filter (/= '\n') . T.pack <$> Prelude.readFile filename
 
 configIO :: IO Config
 configIO =
-  Config <$> (cleverlyReadFile "hook")
-
--- googleConfigIO :: IO Search.Gapi
--- googleConfigIO =
---   Search.config <$> (cleverlyReadFile "google-server-key") <*> (cleverlyReadFile "google-search-engine-id")
+  Config <$> (readSlackFile "hook")
 
 parseText :: Text -> Maybe Text
 parseText text = case T.strip text of
@@ -82,36 +62,36 @@ parseText text = case T.strip text of
 liftMaybe :: Maybe a -> IO a
 liftMaybe = maybe mzero return
 
-messageOfCommand :: Command -> IO Message--IO (Either Servant.Common.Req.ServantError YQLResponse)
+messageOfCommand :: Command -> IO Message
 messageOfCommand (Command "stock" user channel (Just text)) = do
-  --gapi <- liftIO googleConfigIO
   manager <- getGlobalManager
   query <- liftMaybe (parseText text)
-  res <- runClientM (getQuotes (YQLQuery [StockSymbol query]) ) (ClientEnv manager yahooFinanceJsonBaseUrl)
-  --url <- liftMaybe (listToMaybe res)
-  --return res
-  return (messageOf [FormatAt user, FormatString res])
-  where
-    messageOf =
-      FormattedMessage (EmojiIcon "gift") "stockbot" channel
-messageOfCommand _ =
-  mzero
+  res <- runClientM (getQuotes (YQLQuery [StockSymbol query])) (ClientEnv manager yahooFinanceJsonBaseUrl)
+  case res of
+    --Left servantError -> do
+    --  print (servantError)
+    --  return ("<unexpected error>")::IO Message
+    Right quoteLastTradePriceOnly ->
+      return (messageOf [FormatAt user, FormatString (T.pack(show quoteLastTradePriceOnly))])
+      where 
+        messageOf =
+          FormattedMessage(EmojiIcon "gift") "stockbot" channel
 
 stockify :: Maybe Command -> IO Text
 stockify Nothing =
   return "Unrecognized Slack request!"
 
 stockify (Just command) = do
-  putStrLn ("+ Incoming command: " <> show command)
+  Prelude.putStrLn ("+ Incoming command: " <> show command)
   message <- (messageOfCommand) command
   config <- configIO
-  putStrLn ("+ Outgoing message: " <> show (encode <$> message))
+  --putStrLn ("+ Outgoing message: " <> show (message))
   case (debug, message) of
-    (False, Just m) -> do
+    (False,  m) -> do
       _ <- say m config
       return ""
-    (False, Nothing) ->
-      return "error ugh poop"
+    --(False, Nothing) ->
+    --  return "ugh"
     _ ->
       return ""
   where
@@ -120,7 +100,7 @@ stockify (Just command) = do
 
 main :: IO ()
 main = do
-  putStrLn ("+ Listening on port " <> show port)
+  Prelude.putStrLn ("+ Listening on port " <> show port)
   run port (slashSimple stockify)
     where
       port = 3000
